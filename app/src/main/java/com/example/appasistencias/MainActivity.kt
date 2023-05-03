c071c6db18692128d2c0c347221a67899c6462b5
DanielBeltran2 <86611191+DanielBeltran2@users.noreply.github.com>
2023-05-02 21:48:26 -0700

    ya jalo el login y el menu :D

--- app/src/main/java/com/example/appasistencias/MainActivity.kt
+++ app/src/main/java/com/example/appasistencias/MainActivity.kt
@@ -1,11 +1,27 @@
 package com.example.appasistencias
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContentView(R.layout.activity_main)
+        val buttonIngresar: Button =
+            findViewById(R.id.buttonIngresar)
+
+
+        buttonIngresar.setOnClickListener(View.OnClickListener {
+
+            val ingresar = Intent(this, Menu::class.java)
+            startActivity(ingresar)
+
+        })
     }
+
+
 }
