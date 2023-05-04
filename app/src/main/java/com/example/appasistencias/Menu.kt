f84f1ef8d0c380561d1b26ab044bb6d7f32d8daa
daniel.beltran <18170217@itculiacan.edu.mx>
2023-05-03 17:38:55 -0700

    avance de menu

--- app/src/main/java/com/example/appasistencias/Menu.kt
+++ app/src/main/java/com/example/appasistencias/Menu.kt
@@ -1,13 +1,13 @@
 package com.example.appasistencias
 
+import android.content.Intent
 import android.os.Bundle
-import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.example.appasistencias.databinding.ActivityMenuBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class Menu : AppCompatActivity() {
 
@@ -29,6 +29,30 @@ class Menu : AppCompatActivity() {
                 R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications
             )
         )
+        val mOnNavigationItemSelectedListener =
+            BottomNavigationView.OnNavigationItemSelectedListener { item ->
+                when (item.itemId) {
+                    R.id.modificar -> {
+                        val intent1 = Intent(this, ModificarActivity::class.java)
+                        startActivity(intent1)
+                        return@OnNavigationItemSelectedListener true
+                    }
+                    R.id.tomarlista -> {
+                        val intent2 = Intent(this, AgregarActivity::class.java)
+                        startActivity(intent2)
+                        return@OnNavigationItemSelectedListener true
+                    }
+                    R.id.buscar -> {
+                        val intent3 = Intent(this, BuscarActivity::class.java)
+                        startActivity(intent3)
+                        return@OnNavigationItemSelectedListener true
+                    }
+                }
+                false
+            }
+
+
+
 
         navView.setupWithNavController(navController)
     }
